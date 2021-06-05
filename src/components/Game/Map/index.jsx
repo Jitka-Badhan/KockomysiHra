b3d77366118cd57b3c1230e2c9db94871f5dd8f3
Jitka Badhan Nebeska <jitkabadhan@gmail.com>
2021-06-05 17:01:46 +0200

    new commit

--- src/components/Game/Map/index.jsx
+++ src/components/Game/Map/index.jsx
@@ -1,14 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
+import '../data.js';
 
-const Map = () => {
+import Building from '../Building';
+
+const mapItemsIndexes = [
+  {
+    name: 'Hospoda',
+    startX: 549,
+    endX: 654,
+    startY: 409,
+    endY: 437,
+  },
+  {
+    name: 'Knihovna',
+    startX: 321,
+    endX: 468,
+    startY: 288,
+    endY: 353,
+  },
+  {
+    name: 'Obchoďák',
+    startX: 346,
+    endX: 446,
+    startY: 402,
+    endY: 475,
+  },
+  {
+    name: 'Venkovský dům',
+    startX: 640,
+    endX: 714,
+    startY: 279,
+    endY: 331,
+  },
+  {
+    name: 'Továrna',
+    startX: 723,
+    endX: 836,
+    startY: 287,
+    endY: 356,
+  },
+  {
+    name: 'Radnice',
+    startX: 660,
+    endX: 751,
+    startY: 385,
+    endY: 476,
+  },
+  {
+    name: 'Moderní dům',
+    startX: 479,
+    endX: 567,
+    startY: 493,
+    endY: 543,
+  },
+  {
+    name: 'Zahrada',
+    startX: 292,
+    endX: 409,
+    startY: 544,
+    endY: 623,
+  },
+  {
+    name: 'Hřbitov',
+    startX: 668,
+    endX: 836,
+    startY: 548,
+    endY: 633,
+  },
+];
+
+const Map = ({ changeCard }) => {
   const clicked = (e) => {
     const clickedX = e.screenX;
     const clickedY = e.screenY;
 
-    if (clickedX > 585 && clickedX < 737 && clickedY > 301 && clickedY < 401) {
-      console.log('Mám přejít na budovu.');
-    }
+    mapItemsIndexes.forEach((item) => {
+      if (
+        clickedX > item.startX &&
+        clickedX < item.endX &&
+        clickedY > item.startY &&
+        clickedY < item.endY
+      ) {
+        changeCard(<Building />);
+      }
+    });
   };
 
   return (
