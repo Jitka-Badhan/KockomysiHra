2b2eb7e74b1bff68150473d6452be45c1e23f50f
Jitka Badhan Nebeska <jitkabadhan@gmail.com>
2021-06-05 15:31:50 +0200

    neco neco

--- src/components/Game/Map/index.jsx
+++ src/components/Game/Map/index.jsx
@@ -3,8 +3,17 @@ import './style.css';
 
 const Map = () => {
   return (
-
+    <div className="card game__map">
+      <div className="card__content">
+        <div className="map__heading">Město Mňau-chen</div>
+        <img
+          src="/assets/mapka_navrh3.svg"
+          alt="map of Mnau-chen"
+          className="map"
+        />
+      </div>
+    </div>
   );
-}
+};
 
 export default Map;
